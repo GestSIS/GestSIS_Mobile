38eed5c4b4de349df0fe0ac070f668e236651aa8
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-05-23 10:57:40 +0200

    V2.1.3

--- android/build.sh
+++ android/build.sh
@@ -1,12 +1,14 @@
 #/bin/sh
 
 cd /app
+rm -rf dist
 yarn install
+yarn build
 yarn cap sync
 cd /app/android
 chmod 777 gradlew
 ./gradlew assemble
 ./gradlew bundle
 jarsigner -keystore ../keys/gestsis-release-key.jks ./app/build/outputs/bundle/release/app-release.aab gestsis-ks
-mv ./app/build/outputs/bundle/release/app-release.aab ./app/build/outputs/bundle/release/gestsis-2.0.x.aab
+mv ./app/build/outputs/bundle/release/app-release.aab ./app/build/outputs/bundle/release/gestsis-2.1.x.aab
 chmod -R a+rw ./app/build/outputs/bundle/release/
--- src/views/GsParametres.vue
+++ src/views/GsParametres.vue
@@ -162,7 +162,7 @@ const onSelectSis = async (sis: string) => {
 
     <ion-footer>
       <div class="ion-padding copyright">
-        Version 2.1.2
+        Version 2.1.3
         <br />Application créée par Bastien Wermeille <br />support@gestsis.ch
         <br />
         {{ new Date().getFullYear() }} © GestSIS, Tous droits réservés
