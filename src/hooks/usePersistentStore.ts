3476586186751ecaef1ca6122afd11990af84c1a
Bastien Wermeille <bastien.wermeille@gmail.com>
2024-05-05 13:43:47 +0200

    Release v2.5.1

--- src/hooks/usePersistentStore.ts
+++ src/hooks/usePersistentStore.ts
@@ -1,12 +1,16 @@
-import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
-import { Storage, Drivers } from '@ionic/storage';
+import * as CordovaSQLiteDriver from "localforage-cordovasqlitedriver";
+import { Storage, Drivers } from "@ionic/storage";
 
 const persistentStore = new Storage({
-  driverOrder: [CordovaSQLiteDriver._driver, Drivers.IndexedDB, Drivers.LocalStorage]
+  driverOrder: [
+    CordovaSQLiteDriver._driver,
+    Drivers.IndexedDB,
+    Drivers.LocalStorage,
+  ],
 });
 // TODO: See if await should be added !
 persistentStore.create();
 
-export function usePersistentStore(){
-  return { persistentStore }
+export function usePersistentStore() {
+  return { persistentStore };
 }
