c9e2fee4a08f3fe052acb21ac6c89372968d22cf
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-04-11 19:40:56 +0200

    Release 2.6.0

--- android/build.sh
+++ android/build.sh
@@ -10,5 +10,5 @@ chmod 777 gradlew
 ./gradlew assemble
 ./gradlew bundle
 jarsigner -keystore ../keys/gestsis-release-key.jks ./app/build/outputs/bundle/release/app-release.aab gestsis-ks
-mv ./app/build/outputs/bundle/release/app-release.aab ./app/build/outputs/bundle/release/gestsis-2.5.x.aab
+mv ./app/build/outputs/bundle/release/app-release.aab ./app/build/outputs/bundle/release/gestsis-2.6.x.aab
 chmod -R a+rw ./app/build/outputs/bundle/release/
