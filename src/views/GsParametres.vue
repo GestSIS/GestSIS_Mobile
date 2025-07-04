3cef134cf691148b7475ce5c7d389b9c5ed1276f
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-07-04 11:43:18 +0200

    Fix issue with keyboard overlap input

--- src/views/GsParametres.vue
+++ src/views/GsParametres.vue
@@ -165,7 +165,7 @@ const onSelectSis = async (sis: string) => {
 
     <ion-footer>
       <div class="ion-padding copyright">
-        Version 2.6.0
+        Version 2.6.1
         <br />Application créée par Bastien Wermeille <br />support@gestsis.ch
         <br />
         {{ new Date().getFullYear() }} © GestSIS, Tous droits réservés
