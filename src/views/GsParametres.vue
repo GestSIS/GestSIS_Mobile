3430475a7dd51a3faf252f1cacc010b7c95ec5a7
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-07-02 14:51:45 +0200

    v2.6.7

--- src/views/GsParametres.vue
+++ src/views/GsParametres.vue
@@ -183,7 +183,7 @@ const onSelectSis = async (sis: string) => {
 
     <ion-footer>
       <div class="ion-padding copyright">
-        Version 2.6.6
+        Version 2.6.7
         <br />Application créée par Bastien Wermeille <br />support@gestsis.ch
         <br />
         {{ new Date().getFullYear() }} © GestSIS, Tous droits réservés
