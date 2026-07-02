fb41416080ee518af48f4b4854d8cd8eb89fb113
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-07-02 10:34:09 +0200

    Remove unused local-forage storage

--- src/hooks/usePersistentStore.ts
+++ src/hooks/usePersistentStore.ts
@@ -1,12 +1,7 @@
-import * as CordovaSQLiteDriver from "localforage-cordovasqlitedriver";
 import { Storage, Drivers } from "@ionic/storage";
 
 const persistentStore = new Storage({
-  driverOrder: [
-    CordovaSQLiteDriver._driver,
-    Drivers.IndexedDB,
-    Drivers.LocalStorage,
-  ],
+  driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
 });
 const storageReady = persistentStore.create();
 
